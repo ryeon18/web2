442358a5c318421962d71913d56d80c2588265a0
ryeon18 <62207127+ryeon18@users.noreply.github.com>
2021-06-23 10:51:13 +0900

    Add files via upload

--- color.js
+++ color.js
@@ -1,19 +1,22 @@
 var Links={
     setColor: function(color){
-    var alist = document.querySelectorAll('a');
-    var i = 0;
-    while(i < alist.length){
-        alist[i].style.color = color;
-        i = i + 1;
-        }
+    // var alist = document.querySelectorAll('a');
+    // var i = 0;
+    // while(i < alist.length){
+    //     alist[i].style.color = color;
+    //     i = i + 1;
+    //     }
+    $('a').css('color',color);
 }
 }
 var Body ={
     setColor:function(color){
-    document.querySelector('body').style.color = color;
+        $('Body').css('color',color);
+    // document.querySelector('body').style.color = color;
 },
     setBackgroundColor:function(color){
-    document.querySelector('body').style.backgroundColor=color;
+        $('Body').css('backgroundColor',color);
+    // document.querySelector('body').style.backgroundColor=color;
 }
 }
 function nightDayHandler(self){
